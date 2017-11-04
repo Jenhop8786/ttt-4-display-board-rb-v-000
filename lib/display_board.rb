9aa05a6f54aea918ce5a733096c3e6c8aed33adb
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-04 17:00:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,8 @@ board = ["O", "X", "X", " ", "X", " ", " ", " ", " "]
 def display_board(board)
 
 puts " #{board[0]} | "
-puts " #{board[4]} | #{board[2]} | #{board[1]} "
+puts " #{board[4]} | "
+puts " #{board[2]} | "
+puts " #{board[1]}   "
 
 end
